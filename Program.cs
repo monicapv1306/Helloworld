c22f8e511dcbe055bc612307fbbeb1f45fe1c1ba
monica <monica.pvenkatesh@gmail.com>
2025-02-26 10:08:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace Helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("Hello, World!");
+          
+
+            int x;
+            x = 3;
+        }
+    }
+}
